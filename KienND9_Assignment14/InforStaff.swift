74a2246e3fc91ba7a031d3916208092ab25bcc34
Kien Nguyen Dang <kiennguyendang@Kiens-Mac.local>
2016-12-29 14:58:35 +0700

    first commit

---
+++ KienND9_Assignment14/InforStaff.swift
@@ -0,0 +1,9 @@
+//
+//  InforStaff.swift
+//  KienND9_Assignment14
+//
+//  Created by Kien Nguyen Dang on 12/3/16.
+//  Copyright © 2016 Kien Nguyen Dang. All rights reserved.
+//
+
+import Foundation
---
+++ Staff/Staff.swift
@@ -0,0 +1,9 @@
+//
+//  Staff.swift
+//  KienND9_Assignment14
+//
+//  Created by Kien Nguyen Dang on 12/2/16.
+//  Copyright © 2016 Kien Nguyen Dang. All rights reserved.
+//
+
+import Foundation
